d290ab75496baa3bdec0a78423f6c9a66e6a6bf7
shafirpl <shafi.rpl@gmail.com>
2018-11-22 13:55:28 -0800

    Edit user backend added

--- BackEnd/BackEnd/routes/editUser.js
+++ BackEnd/BackEnd/routes/editUser.js
@@ -4,6 +4,7 @@ var router                = express.Router();
 var posting               = require("../database/posting");
 var User                  = require("../database/users");
 var passport              = require("passport");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 //router
 //getting all the information about the username and sending it to the frontend
@@ -21,8 +22,27 @@ router.post("/user",function(req,res){
 
 
 //updating the user profile
-router.put("/user/edit",function(req,res){
-  //User.findOneAndUpdate({'username':req.body.username},function(err,foundUser));
-  res.send("hello");
+router.post("/user/edit",function(req,res){
+  User.findOne({'username':req.body.username},function(err,foundUser){
+    if(err){
+      console.log(err);
+      res.send({"success":false,"message":"Err in findOne function"});
+      return;
+    }
+    foundUser.setPassword(req.body.password,function(err){
+      if(err){
+        console.log(err);
+        res.send({"success":false, "message": "Reset password failed"});
+        return;
+      }
+      foundUser.email = req.body.email;
+      foundUser.phonenumber = req.body.phonenumber;
+      foundUser.save();
+      res.send({"success": true, "message": "Successfully updated user"});
+      return;
+    });
+
+  });
+  //res.send("hello");
 });
 module.exports = router;
