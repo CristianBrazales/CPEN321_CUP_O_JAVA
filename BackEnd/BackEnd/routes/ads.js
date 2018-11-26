3bbd1f48449514752921ae30d2dec38cfe09a182
Skylar <skylarliang233@gmail.com>
2018-11-25 21:13:06 -0800

    not using formidable

--- BackEnd/BackEnd/routes/ads.js
+++ BackEnd/BackEnd/routes/ads.js
@@ -12,20 +12,16 @@ var validator             = require("../helperFunctions/validator.js");
 //   url:'mongodb://localhost:27017/rental_tinder_database', useNewUrlParser: true 
 // });
 
-var formidable = require('express-formidable');
-//router.use(formidable());
-router.use(formidable({
-    //encoding: 'utf-8',
-    uploadDir: './uploads/',
-    multiples: true, // req.files to be arrays of files
-  }));
+// var formidable = require('express-formidable');
+// //router.use(formidable());
+// router.use(formidable({
+//     //encoding: 'utf-8',
+//     uploadDir: './uploads/',
+//     multiples: true, // req.files to be arrays of files
+//   }));
 //--------------------
 
 var escapeStringRegexp    = require('escape-string-regexp');
-
-
-
-
 
 //creating a new add for the current user
 router.post("/posting", 
@@ -33,62 +29,60 @@ router.post("/posting",
 //upload.single('photo'), 
 //--------------------
 function(req,res){
-  //----------------------------
-  //req.fields = req.fields;
-  //----------------------------
-  var newPosting = new posting({username:req.fields.username});
-  newPosting.address = req.fields.address;
+
+  var newPosting = new posting({username:req.body.username});
+  newPosting.address = req.body.address;
   console.log("Room number from node backend:");
-  console.log(req.fields.roomNumber);
+  console.log(req.body.roomNumber);
 
   //validating room Number
-  var validRoomNumber = validator.validateRoomNumber(req.fields.roomNumber);
+  var validRoomNumber = validator.validateRoomNumber(req.body.roomNumber);
 
   if(!validRoomNumber){
     res.send({"success":false, "message":"Invalid room number"});
     return;
   }
 
-  // if(req.fields.roomNumber<0){
+  // if(req.body.roomNumber<0){
   //
   //   res.send({"success":false, "message":"Invalid room number"});
   //   return;
   // }
 
 
-  newPosting.roomNumber = req.fields.roomNumber;
+  newPosting.roomNumber = req.body.roomNumber;
 
 
   //validate zip code
 
-  var isValidZipcode = validator.validateZipCode(req.fields.zipcode);
+  var isValidZipcode = validator.validateZipCode(req.body.zipcode);
     if(!isValidZipcode) {
     res.send({"success":false, "message":"Invalid zip code"});
     return;
   }
 
-  // var isValidZipcode = postcode.validate(req.fields.zipcode, 'CA');
+  // var isValidZipcode = postcode.validate(req.body.zipcode, 'CA');
   // if(!isValidZipcode) {
   //   res.send({"success":false, "message":"Invalid zip code"});
   //   return;
   // }
 
   //creating an object for search query
-  newPosting.zipcode = req.fields.zipcode.toLowerCase();
-  newPosting.smoke = req.fields.smoke;
-  newPosting.earlyMorningPerson = req.fields.earlyMorningPerson;
-  newPosting.partyPerson = req.fields.partyPerson;
-  newPosting.title = req.fields.title;
+  newPosting.zipcode = req.body.zipcode.toLowerCase();
+  newPosting.smoke = req.body.smoke;
+  newPosting.earlyMorningPerson = req.body.earlyMorningPerson;
+  newPosting.partyPerson = req.body.partyPerson;
+  newPosting.title = req.body.title;
   //-------------------------
   //console.log("req file in ad.js: "+ req.file);
   //newPosting.image.data = req.file.buffer;
   //newPosting.image.contentType = req.file.mimetype;
-  //newPosting.image64 = req.fields.photo;
+  //newPosting.image64 = req.body.photo;
   newPosting.img.path = req.files.photo.path;
 
   //-------------------------
-  //newPosting.photo = req.fields.photo;
-  newPosting.description = req.fields.description;
+  //newPosting.photo = req.body.photo;
+  newPosting.description = req.body.description;
   //trying to post this data to the database
   posting.create(newPosting,function(err,returnedRoom){
     //error handling
